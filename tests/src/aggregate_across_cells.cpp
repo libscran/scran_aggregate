bdab3c229ae318e7afee105450bf89fabd8fecc6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-01-07 17:16:22 -0800

    Switch to LocalOutputBuffers for column-based aggregate_across_cells.
    This avoids false sharing between threads and also zeros the
    user-supplied buffers before performing any additions. The latter fixes
    a bug where values were incorrectly added to uninitialized buffers. It
    comes at a cost of adding the tatami_stats dependency, though.

--- tests/src/aggregate_across_cells.cpp
+++ tests/src/aggregate_across_cells.cpp
@@ -2,10 +2,11 @@
 
 #include "scran_aggregate/aggregate_across_cells.hpp"
 #include "scran_tests/scran_tests.hpp"
+
 #include <map>
 #include <random>
 
-std::vector<int> create_groupings(size_t n, int ngroups) {
+static std::vector<int> create_groupings(size_t n, int ngroups) {
     std::vector<int> groupings(n);
     for (size_t g = 0; g < groupings.size(); ++g) {
         groupings[g] = g % ngroups;
@@ -114,4 +115,52 @@ TEST(AggregateAcrossCells, Skipping) {
     auto skipped = scran_aggregate::aggregate_across_cells(*input, grouping.data(), opt);
     EXPECT_EQ(skipped.sums.size(), 0);
     EXPECT_EQ(skipped.detected.size(), 0);
+}
+
+TEST(AggregateAcrossCells, DirtyBuffers) {
+    int nr = 88, nc = 126;
+    auto vec = scran_tests::simulate_vector(nr * nc, []{
+        scran_tests::SimulationParameters sparams;
+        sparams.density = 0.1;
+        sparams.seed = 69;
+        return sparams;
+    }());
+
+    tatami::DenseRowMatrix<double, int> dense_row(nr, nc, std::move(vec));
+    auto sparse_column = tatami::convert_to_compressed_sparse(&dense_row, false);
+    size_t ngroups = 3;
+    auto grouping = create_groupings(dense_row.ncol(), ngroups);
+
+    // Setting up some dirty buffers.
+    scran_aggregate::AggregateAcrossCellsResults<double, int> store;
+    scran_aggregate::AggregateAcrossCellsBuffers<double, int> buffers;
+    store.sums.resize(ngroups, std::vector<double>(nr, -1));
+    store.detected.resize(ngroups, std::vector<int>(nr, -1));
+    buffers.sums.resize(ngroups);
+    buffers.detected.resize(ngroups);
+    for (size_t l = 0; l < ngroups; ++l) {
+        buffers.sums[l] = store.sums[l].data();
+        buffers.detected[l] = store.detected[l].data();
+    }
+
+    scran_aggregate::AggregateAcrossCellsOptions opt;
+    auto ref = scran_aggregate::aggregate_across_cells(dense_row, grouping.data(), opt);
+    scran_aggregate::aggregate_across_cells(dense_row, grouping.data(), buffers, opt);
+    EXPECT_EQ(ref.sums.size(), store.sums.size());
+    EXPECT_EQ(ref.detected.size(), store.detected.size());
+    for (size_t l = 0; l < ngroups; ++l) {
+        EXPECT_EQ(ref.sums[l], store.sums[l]);
+        EXPECT_EQ(ref.detected[l], store.detected[l]);
+    }
+
+    // Same for column-major iteration.
+    for (size_t l = 0; l < ngroups; ++l) {
+        std::fill_n(buffers.sums[l], nr, -1);
+        std::fill_n(buffers.detected[l], nr, -1);
+    }
+    scran_aggregate::aggregate_across_cells(*sparse_column, grouping.data(), buffers, opt);
+    for (size_t l = 0; l < ngroups; ++l) {
+        EXPECT_EQ(ref.sums[l], store.sums[l]);
+        EXPECT_EQ(ref.detected[l], store.detected[l]);
+    }
 }
