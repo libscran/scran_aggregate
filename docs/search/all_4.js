d35e91ff14a3b30060e26b75b6e58bdb6a4dee11
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-11-11 23:29:16 +0000

    Deploying to gh-pages from @ libscran/scran_aggregate@11e8017ac52a876e32631f20f47cf53057b2569d 🚀

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['elementtype_0',['ElementType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a83f5bea1c12d21f81732d6abd0665554',1,'tatami']]],
+  ['elementtype_0',['ElementType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a7676f28b5618fd937e38b7a5c759d8bf',1,'tatami']]],
   ['elementtype_2ehpp_1',['ElementType.hpp',['https://tatami-inc.github.io/tatami/ElementType_8hpp.html',1,'']]],
-  ['end_2',['end',['https://tatami-inc.github.io/tatami/classtatami_1_1ArrayView.html#a904e6d03b59a20f2cc7831fbb318cf33',1,'tatami::ArrayView::end()'],['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html#add771080790c65d2943c54a0615ee136',1,'tatami::SomeNumericArray::end()']]],
+  ['end_2',['end',['https://tatami-inc.github.io/tatami/classtatami_1_1ArrayView.html#a15127c10722fd3483010f38231dd7ee9',1,'tatami::ArrayView::end()'],['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html#add771080790c65d2943c54a0615ee136',1,'tatami::SomeNumericArray::end()']]],
   ['expression_20values_20across_20cells_3',['Aggregate expression values across cells',['../index.html',1,'']]],
   ['extractor_2ehpp_4',['Extractor.hpp',['https://tatami-inc.github.io/tatami/Extractor_8hpp.html',1,'']]]
 ];
--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['reference_0',['reference',['https://tatami-inc.github.io/tatami/structtatami_1_1SomeNumericArray_1_1Iterator.html#a4bf2843ca912061c3cc0400bbb75abe6',1,'tatami::SomeNumericArray::Iterator']]],
-  ['resize_5fcontainer_5fto_5findex_5fsize_1',['resize_container_to_Index_size',['https://tatami-inc.github.io/tatami/namespacetatami.html#a5c8a6fad0ecaa74e7eb69d50c25c44f8',1,'tatami']]],
-  ['retrieve_5fcompressed_5fsparse_5fcontents_2',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a05dc21541e6db5a8896049a0647c4230',1,'tatami']]],
-  ['retrieve_5ffragmented_5fsparse_5fcontents_3',['retrieve_fragmented_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a5c71daf9bdb1ea8d3e16f35c5b15facc',1,'tatami']]],
+  ['resize_5fcontainer_5fto_5findex_5fsize_1',['resize_container_to_Index_size',['https://tatami-inc.github.io/tatami/namespacetatami.html#a61b43d2b751c7fcca6815f3be929d4d3',1,'tatami']]],
+  ['retrieve_5fcompressed_5fsparse_5fcontents_2',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a7e0ab534fd3195a57abcb47272d41ded',1,'tatami']]],
+  ['retrieve_5ffragmented_5fsparse_5fcontents_3',['retrieve_fragmented_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a4b4feed54780ff0d6ec18a38a5752f22',1,'tatami']]],
   ['retrievecompressedsparsecontentsoptions_4',['RetrieveCompressedSparseContentsOptions',['https://tatami-inc.github.io/tatami/structtatami_1_1RetrieveCompressedSparseContentsOptions.html',1,'tatami']]],
   ['retrievefragmentedsparsecontentsoptions_5',['RetrieveFragmentedSparseContentsOptions',['https://tatami-inc.github.io/tatami/structtatami_1_1RetrieveFragmentedSparseContentsOptions.html',1,'tatami']]]
 ];
