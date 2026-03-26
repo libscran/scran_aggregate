d911dcb0af7709ff00c3e9823d90009e55fe6577
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-03-26 11:15:55 +0000

    Deploying to gh-pages from @ libscran/scran_aggregate@bf7a87bb5fb7cbaeb6117eab2fa71e4507eb75ae 🚀

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -3,11 +3,12 @@ var searchData=
   ['fillcompressedsparsecontentsoptions_0',['FillCompressedSparseContentsOptions',['https://tatami-inc.github.io/tatami/structtatami_1_1FillCompressedSparseContentsOptions.html',1,'tatami']]],
   ['fixedvectororacle_1',['FixedVectorOracle',['https://tatami-inc.github.io/tatami/classtatami_1_1FixedVectorOracle.html',1,'tatami']]],
   ['fixedvieworacle_2',['FixedViewOracle',['https://tatami-inc.github.io/tatami/classtatami_1_1FixedViewOracle.html',1,'tatami']]],
-  ['fragmentedsparsecolumnmatrix_3',['FragmentedSparseColumnMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseColumnMatrix.html',1,'tatami']]],
-  ['fragmentedsparsecontents_4',['FragmentedSparseContents',['https://tatami-inc.github.io/tatami/structtatami_1_1FragmentedSparseContents.html',1,'tatami']]],
-  ['fragmentedsparsematrix_5',['FragmentedSparseMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
-  ['fragmentedsparsematrix_3c_20value_5f_2c_20index_5f_2c_20valuevectorstorage_5f_2c_20indexvectorstorage_5f_20_3e_6',['FragmentedSparseMatrix&lt; Value_, Index_, ValueVectorStorage_, IndexVectorStorage_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
-  ['fragmentedsparsematrixoptions_7',['FragmentedSparseMatrixOptions',['https://tatami-inc.github.io/tatami/structtatami_1_1FragmentedSparseMatrixOptions.html',1,'tatami']]],
-  ['fragmentedsparserowmatrix_8',['FragmentedSparseRowMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseRowMatrix.html',1,'tatami']]],
-  ['fullsparsifiedwrapper_9',['FullSparsifiedWrapper',['https://tatami-inc.github.io/tatami/classtatami_1_1FullSparsifiedWrapper.html',1,'tatami']]]
+  ['forceddense_3',['ForcedDense',['https://tatami-inc.github.io/tatami/classtatami_1_1ForcedDense.html',1,'tatami']]],
+  ['fragmentedsparsecolumnmatrix_4',['FragmentedSparseColumnMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseColumnMatrix.html',1,'tatami']]],
+  ['fragmentedsparsecontents_5',['FragmentedSparseContents',['https://tatami-inc.github.io/tatami/structtatami_1_1FragmentedSparseContents.html',1,'tatami']]],
+  ['fragmentedsparsematrix_6',['FragmentedSparseMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
+  ['fragmentedsparsematrix_3c_20value_5f_2c_20index_5f_2c_20valuevectorstorage_5f_2c_20indexvectorstorage_5f_20_3e_7',['FragmentedSparseMatrix&lt; Value_, Index_, ValueVectorStorage_, IndexVectorStorage_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseMatrix.html',1,'tatami']]],
+  ['fragmentedsparsematrixoptions_8',['FragmentedSparseMatrixOptions',['https://tatami-inc.github.io/tatami/structtatami_1_1FragmentedSparseMatrixOptions.html',1,'tatami']]],
+  ['fragmentedsparserowmatrix_9',['FragmentedSparseRowMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1FragmentedSparseRowMatrix.html',1,'tatami']]],
+  ['fullsparsifiedwrapper_10',['FullSparsifiedWrapper',['https://tatami-inc.github.io/tatami/classtatami_1_1FullSparsifiedWrapper.html',1,'tatami']]]
 ];
--- docs/search/files_5.js
+++ docs/search/files_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['fixedoracle_2ehpp_0',['FixedOracle.hpp',['https://tatami-inc.github.io/tatami/FixedOracle_8hpp.html',1,'']]],
-  ['fragmentedsparsematrix_2ehpp_1',['FragmentedSparseMatrix.hpp',['https://tatami-inc.github.io/tatami/FragmentedSparseMatrix_8hpp.html',1,'']]]
+  ['forceddense_2ehpp_1',['ForcedDense.hpp',['https://tatami-inc.github.io/tatami/ForcedDense_8hpp.html',1,'']]],
+  ['fragmentedsparsematrix_2ehpp_2',['FragmentedSparseMatrix.hpp',['https://tatami-inc.github.io/tatami/FragmentedSparseMatrix_8hpp.html',1,'']]]
 ];
